1a359a43bf7cc6382755a7069abeb539284d6309
sanjay9 <sanjaymahabir9@gmail.com>
2023-03-15 02:12:39 -0400

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
+import React from "react";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import NavBar from "./NavBar";
+import Register from "./userAccounts/Register";
+import BookUpload from "./BookUpload"
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+      </div>
+      <div style={{ margin: "90px" }}>
+        <Routes>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/upload" element={<BookUpload />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
