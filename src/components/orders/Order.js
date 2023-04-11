37872d43a4a2b0db2ea67527aff972d0824189f2
Charlie Bi <charlie_bi@hotmail.com>
2023-04-11 18:18:26 -0400

    added delete button

--- src/components/orders/Order.js
+++ src/components/orders/Order.js
@@ -3,9 +3,12 @@ import Card from "react-bootstrap/Card";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
+import accountContext from "../userAccounts/accountContext";
 
-export default function Order({ order,setOrders }) {
+export default function Order({ order, setOrders }) {
   let navigate = useNavigate();
+
+  const { userType } = React.useContext(accountContext);
 
   function deleteRecord() {
     axios
@@ -20,30 +23,43 @@ export default function Order({ order,setOrders }) {
   }
 
   async function updateStatus(status, id) {
-   try{
-    await axios.put(`http://localhost:3500/orders/statusUpdate/${id}`, {status})
+    try {
+      await axios.put(`http://localhost:3500/orders/statusUpdate/${id}`, {
+        status,
+      });
 
-    axios
-      .get("http://localhost:3500/orders")
-      .then((res) => setOrders(res.data))
-      .catch((err) => console.error(err));
-
-
-  }catch(err){
-    console.log(err);
+      axios
+        .get("http://localhost:3500/orders")
+        .then((res) => setOrders(res.data))
+        .catch((err) => console.error(err));
+    } catch (err) {
+      console.log(err);
+    }
   }
-  }
-
 
   return (
     <Card style={{ width: "80%", padding: "30dp" }}>
       <Card.Body>
         <Card.Title>Order ID: {order._id}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">
-          Buyer: {order.buyerEmail}
+          Buyer: {order.buyerEmail}{" "}
+          <Button
+            href={`mailto:${order.buyerEmail}?Subject=Bookepedia%20Order%20`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Email Seller
+          </Button>
         </Card.Subtitle>
         <Card.Subtitle className="mb-2 text-muted">
           Seller: {order.sellerEmail}
+          <Button
+            href={`mailto:${order.sellerEmail}?Subject=Bookepedia%20Order%20`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Email Buyer
+          </Button>
         </Card.Subtitle>
         <Card.Subtitle className="mb-2 text-muted">
           Condition Guarantee: {order.conditionVerification}
@@ -54,22 +70,28 @@ export default function Order({ order,setOrders }) {
         <Button variant="primary" value={order._id} onClick={handleEdit}>
           Edit
         </Button>{" "}
-        <Button variant="primary" onClick={() => updateStatus("Shipped",order._id)}>
+        <Button
+          variant="primary"
+          onClick={() => updateStatus("Shipped", order._id)}
+        >
           Shipped
         </Button>{" "}
-        <Button variant="primary" onClick={() => updateStatus("Delivered",order._id)}>
+        <Button
+          variant="primary"
+          onClick={() => updateStatus("Delivered", order._id)}
+        >
           Delivered
         </Button>
-        {/* <Button variant="danger" onClick={deleteRecord}>
-          Delete
-        </Button> */}
-        <br/><br/>
-        <Button href={`mailto:${order.buyerEmail}?Subject=Bookepedia%20Order%20`} target="_blank" rel="noopener noreferrer">
-      Email Buyer
-    </Button>
+        <br />
+        <br />
+        {userType === "DELIVERY" ? (
+          <Button variant="danger" onClick={deleteRecord}>
+            Delete
+          </Button>
+        ) : (
+          <div></div>
+        )}
       </Card.Body>
-      
     </Card>
-    
   );
 }
